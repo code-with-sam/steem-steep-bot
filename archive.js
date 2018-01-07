3c23faebc67eeb17689960db117b4edaddae7576
Sam Billingham <sambillie.sb@gmail.com>
2018-01-07 20:01:16 +0000

    add schedule example

--- archive.js
+++ archive.js
@@ -1,12 +1,30 @@
 let schedule = [
-  {
-    date : '07/01/2018',
-    dateFormat : 'DD/MM/YYYY',
-    title : 'Incredible View From the Grand Canyon',
-    description : 'body text',
-    tags : ['photography', 'life', 'blog', 'steepshot'],
-    imageLink : 'http://linktoimage.org/04387650431',
-    imageSize: { height:560, width:560 }
-  }
+    {
+      date : '07/01/2018',
+      dateFormat : 'DD/MM/YYYY',
+      title : 'Exploring The Black Sand Beach',
+      description : 'adventure 001',
+      tags : ['photography', 'iceland'],
+      imageLink : 'https://i.imgsafe.org/26/262efd1c8b.jpeg',
+      imageSize: { height:800, width:800 }
+    },
+    {
+      date : '08/01/2018',
+      dateFormat : 'DD/MM/YYYY',
+      title : 'Beach Vibes',
+      description : 'Adventure 002',
+      tags : ['photography', 'california'],
+      imageLink : 'https://i.imgsafe.org/26/262efd1c8b.jpeg',
+      imageSize: { height:800, width:800 }
+    },
+    {
+      date : '09/01/2018',
+      dateFormat : 'DD/MM/YYYY',
+      title : 'Hong Kong Mission ',
+      description : 'adventure 003 ',
+      tags : ['photography', 'hk'],
+      imageLink : 'https://i.imgsafe.org/26/262efd1c8b.jpeg',
+      imageSize: { height:800, width:800 }
+    }
 ]
 module.exports = schedule
