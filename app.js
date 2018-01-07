943cd7925c1608f43910d335e46aba9408f4ac12
Sam Billingham <sambillie.sb@gmail.com>
2018-01-07 17:10:23 +0000

    add scheduler

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const steepshot = require('./post');
+const schedule = require('node-schedule');
 
-steepshot.post({
-  uniqueUrlString: '',
-  commentTitle: '',
-  beneficiaries: [],
-  body: `post body here`,
-  postBody: '',
-  tags: ['photography', 'life'],
-  imageSize: { height:560, width:560}
-})
+// 10am everyday
+let timer = schedule.scheduleJob('* 10 * * *', function(){
+
+
+
+  steepshot.post({
+    uniqueUrlString: '',
+    commentTitle: '',
+    beneficiaries: [],
+    body: `post body here`,
+    postBody: '',
+    tags: ['photography', 'life'],
+    imageSize: { height:560, width:560}
+  })
+
+});
